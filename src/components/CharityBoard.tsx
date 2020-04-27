993e88505af8bb0f8effcb21f4ddf4300f5cc1c5
Alex Lin <alex@yetihq.com>
2020-04-26 23:20:29 -0500

    pulling a list of charities

--- src/components/CharityBoard.tsx
+++ src/components/CharityBoard.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { request } from 'graphql-request';
 
 import CharityTile from './CharityTile';
 
@@ -23,9 +24,36 @@ const charityData = [
         notes: 'Hello',
         archived: false
     },
-]
+];
 
 const CharityBoard = (props: any) => {
+    
+    const [charityData, setCharityData] = useState([]);
+
+    useEffect(() => {
+        const query = `{
+            organizations(stage: PUBLISHED, where: {archived: false}) {
+              id
+              name
+              notes
+              type
+              url
+              contact
+              description {
+                markdown
+              }
+              image {
+                url
+              }
+            }
+          }`
+          
+          request('https://api-us-west-2.graphcms.com/v2/ck9g8q8ub07ra01wa6mz55xtl/master', query).then(data => {
+                // console.log(data)
+                setCharityData(data.organizations)
+            }
+          );
+    });
 
 
     return (
@@ -33,8 +61,8 @@ const CharityBoard = (props: any) => {
             <div>Charity Board</div>
             <div className='tile-container'>
                 {
-                    charityData.map(organization => {
-                        return <CharityTile organization={organization}/>;
+                    charityData.map((organization, index) => {
+                        return <CharityTile key={index} organization={organization}/>;
                     })
                 }
             </div>
